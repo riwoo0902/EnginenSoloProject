3c6026a09e6fbb096f360fc7fe5814cc20384d48
riwoo0902 <imliu4342@gmail.com>
2026-01-04 22:23:25 +0900

    save

--- Assets/1.Script/EntityScript/Entity.cs
+++ Assets/1.Script/EntityScript/Entity.cs
@@ -1,12 +1,23 @@
+using System;
+using _1.Script.CollisionManagerScript;
 using UnityEngine;
 
 namespace _1.Script.EntityScript
 {
-    public class Entity : MonoBehaviour
+    [RequireComponent(typeof(Collider))]
+    public class Entity : MonoBehaviour,ICanCollision
     {
+        private Collider _collider;
         
-        
-        
-        
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+            CollisionManage<Entity>.TryAddPool(_collider, this);
+        }
+
+        private void OnDestroy()
+        {
+            CollisionManage<Entity>.RemovePool(_collider);
+        }
     }
 }
