1c24bd8777f83c37316187f2064bc93a345e79bc
riwoo0902 <imliu4342@gmail.com>
2026-01-20 19:18:45 +0900

    feat:TeamEnum

--- Assets/1.Script/EntityScript/Entities/Entity.cs
+++ Assets/1.Script/EntityScript/Entities/Entity.cs
@@ -1,11 +1,14 @@
 using _1.Script.EntityScript.Module;
 using _1.Script.Systems.EventBusScript;
 using _1.Script.Systems.EventBusScript.Events;
+using UnityEngine;
 
 namespace _1.Script.EntityScript.Entities
 {
     public abstract class Entity : ModuleOwner
     {
+        public Team myTeam;
+        
         protected override void Awake()
         {
             base.Awake();
@@ -13,6 +16,8 @@ namespace _1.Script.EntityScript.Entities
             EventBus<EntitySpawn>.Raise(new EntitySpawn(this));
             
         }
+
+        public bool IsTeam(Team team) => myTeam == team;
         
         
     }
---
+++ Assets/1.Script/EntityScript/Entities/Team.cs
@@ -0,0 +1,8 @@
+namespace _1.Script.EntityScript.Entities
+{
+    public enum Team
+    {
+        Red,Blue,Yellow
+    }
+    
+}
