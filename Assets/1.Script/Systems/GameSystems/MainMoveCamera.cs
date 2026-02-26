8b18e59ecef8b5d9f3eef0ab27882548e1e67232
riwoo0902 <imliu4342@gmail.com>
2026-02-26 23:38:30 +0900

    feat:Camera confiner,fix:Camera move

--- Assets/1.Script/Systems/GameSystems/MainMoveCamera.cs
+++ Assets/1.Script/Systems/GameSystems/MainMoveCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using _10.InputSystem;
 using _2.So._1.Scripts;
 using _2.So._1.Scripts.EventChannels;
@@ -5,20 +6,26 @@ using UnityEngine;
 
 namespace _1.Script.Systems.GameSystems
 {
-    [RequireComponent(typeof(Camera))]
     public class MainMoveCamera : MonoSingleton<MainMoveCamera>
     {
         [SerializeField] private InputSO inputSo;
         [SerializeField] private EventChannel uiChannel;
-        [SerializeField] private float moveSpeed = 1;
+        [SerializeField] private float moveSpeed = 50;
+        [SerializeField] private Transform mainCameraTrm;
+
+
+        private void FixedUpdate()
+        {
+            transform.position = mainCameraTrm.position;
+        }
 
         private void Update()
         {
-            Vector2 moveVec = inputSo.CameraMoveDir * moveSpeed;
+            Vector2 moveVec = inputSo.CameraMoveDir * (moveSpeed * Time.deltaTime);
             transform.transform.position += new Vector3(moveVec.x, 0, moveVec.y);
             uiChannel.RaiseEvent(UIEvents.CameraMove.Init(moveVec));
+            
         }
-        
         
     }
 }
