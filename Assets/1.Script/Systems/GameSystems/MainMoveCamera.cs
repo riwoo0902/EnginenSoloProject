5dafe06988083c611c006761d35128f5b32d25c5
riwoo0902 <imliu4342@gmail.com>
2026-02-26 23:23:35 +0900

    feat:CameraMove

--- Assets/1.Script/Systems/GameSystems/MainMoveCamera.cs
+++ Assets/1.Script/Systems/GameSystems/MainMoveCamera.cs
@@ -1,4 +1,6 @@
 using _10.InputSystem;
+using _2.So._1.Scripts;
+using _2.So._1.Scripts.EventChannels;
 using UnityEngine;
 
 namespace _1.Script.Systems.GameSystems
@@ -7,6 +9,15 @@ namespace _1.Script.Systems.GameSystems
     public class MainMoveCamera : MonoSingleton<MainMoveCamera>
     {
         [SerializeField] private InputSO inputSo;
+        [SerializeField] private EventChannel uiChannel;
+        [SerializeField] private float moveSpeed = 1;
+
+        private void Update()
+        {
+            Vector2 moveVec = inputSo.CameraMoveDir * moveSpeed;
+            transform.transform.position += new Vector3(moveVec.x, 0, moveVec.y);
+            uiChannel.RaiseEvent(UIEvents.CameraMove.Init(moveVec));
+        }
         
         
     }
