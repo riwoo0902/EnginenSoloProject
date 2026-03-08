08f548f5cc2ac86b0059fa2d8866b97e950f3c08
riwoo0902 <imliu4342@gmail.com>
2026-03-08 18:50:48 +0900

    save

---
+++ Assets/1.Script/EntityScript/Entities/Modules/AttackSystem/SkillSystem/Skills/TestSkill.cs
@@ -0,0 +1,10 @@
+namespace _1.Script.EntityScript.Entities.Modules.AttackSystem.SkillSystem.Skills
+{
+    public class TestSkill : AbstractSkill
+    {
+        public override void UseSkill()
+        {
+            
+        }
+    }
+}
