322872db39a9a0171d24a7db77db4cf3dcfcd302
riwoo0902 <imliu4342@gmail.com>
2026-03-10 08:28:12 +0900

    feat:Dead,ControlUI

--- Assets/1.Script/UI/ControlUI/ControlUIManager.cs
+++ Assets/1.Script/UI/ControlUI/ControlUIManager.cs
@@ -1,5 +1,5 @@
-using System;
 using _1.Script.EntityScript.Entities.FSM;
+using _10.InputSystem;
 using _2.So._1.Scripts;
 using _2.So._1.Scripts.EventChannels;
 using UnityEngine;
@@ -8,14 +8,16 @@ namespace _1.Script.UI.ControlUI
 {
     public class ControlUIManager : MonoBehaviour
     {
+        [SerializeField] private InputSO inputSo;
         [SerializeField] private EventChannel uiChannel;
-
+        
         private ControlChangeButton[] _buttons;
         private void Awake()
         {
             _buttons = GetComponentsInChildren<ControlChangeButton>();
             foreach (ControlChangeButton controlChangeButton in _buttons)
             {
+                controlChangeButton.inputSo = inputSo;
                 controlChangeButton.OnChangeState += ChangeControlType;
             }
         }
