37b460aa5806839fba379de415f0699b543a2935
riwoo0902 <imliu4342@gmail.com>
2026-03-11 00:29:36 +0900

    save

--- Assets/1.Script/EntityScript/Entities/UnitScript/Units/BuildingUnits/SpawnBuildingUnit.cs
+++ Assets/1.Script/EntityScript/Entities/UnitScript/Units/BuildingUnits/SpawnBuildingUnit.cs
@@ -1,7 +1,17 @@
+using UnityEngine;
+
 namespace _1.Script.EntityScript.Entities.UnitScript.Units.BuildingUnits
 {
     public class SpawnBuildingUnit : BuildingUnit
     {
-        
+        [SerializeField] private GameObject _unitPrefab;
+        [field:SerializeField] public float Delay { get; private set; } = 3;
+
+
+        public void SpawnUnit()
+        {
+            GameObject unit = Instantiate(_unitPrefab);
+            
+        }
     }
 }
