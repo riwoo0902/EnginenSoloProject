b26f97401ba7c89ad87683056025a5320b924759
riwoo0902 <imliu4342@gmail.com>
2026-03-19 17:14:59 +0900

    save

--- Assets/1.Script/EntityScript/Entities/UnitScript/Units/MoveUnits/Player.cs
+++ Assets/1.Script/EntityScript/Entities/UnitScript/Units/MoveUnits/Player.cs
@@ -1,3 +1,4 @@
+using _2.So._1.Scripts.EventChannels;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +8,25 @@ namespace _1.Script.EntityScript.Entities.UnitScript.Units.MoveUnits
     {
         public static readonly Team PlayerTeam = Team.Blue;
         
+        private float _lastTime = 0;
+        private const float Delay = 0.5f;
+        
         protected override void Awake()
         {
             myTeam = PlayerTeam;
             base.Awake();
             healthModule.OnDead += Dead;
+        }
+        
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (Time.time - _lastTime > Delay)
+            {
+                _lastTime = Time.time;
+                entityChannel.RaiseEvent(EntityEvents.PlayerMove.Init(transform.position));
+            }
+            
         }
 
         protected override void OnDestroy()
